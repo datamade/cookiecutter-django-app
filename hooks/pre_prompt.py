7924e55bfcd14aa87472282963765f5ec19a3ff1
Hannah Cushman Garland <hannah.cushman@datamade.us>
2024-10-30 15:08:48 -0500

    Read Node version correctly

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -10,8 +10,12 @@ if __name__ == "__main__":
 
     with open(variable_definition, "r") as f:
         variables = json.load(f)
+
         variables["python_version"] = platform.python_version()
-        node_version = subprocess.run(["node", "-v"], capture_output=True, check=True)
+
+        node_version = subprocess.run(
+            ["node", "-v"], capture_output=True, check=True
+        ).stdout.decode("utf-8")
         variables["node_version"] = re.search(r"\d+", node_version).group(0)
 
     with open(variable_definition, "w") as f:
