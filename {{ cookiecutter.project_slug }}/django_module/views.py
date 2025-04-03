61df83d140c0abb724c49c804cf7c60789ed0286
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2025-04-03 15:56:46 -0400

    adjust 404/500 template paths

--- {{ cookiecutter.project_slug }}/django_module/views.py
+++ {{ cookiecutter.project_slug }}/django_module/views.py
@@ -19,9 +19,9 @@ def robots_txt(request):
     )
 
 
-def page_not_found(request, exception, template_name="{{ cookiecutter.module_name }}/404.html"):
+def page_not_found(request, exception, template_name="404.html"):
     return render(request, template_name, status=404)
 
 
-def server_error(request, template_name="{{ cookiecutter.module_name }}/500.html"):
+def server_error(request, template_name="500.html"):
     return render(request, template_name, status=500)
